9411b954bb26bcbf8af48cf210f527028ef76b98
Filipe Pina <fopina@gmail.com>
2015-06-25 17:56:49 +0100

    setuptools

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='py-tgbot',
+    version='0.1',
+    packages=['tgbot'],
+    include_package_data=True,
+    license='MIT License',
+    description='A wrapper for Telegram Bot API.',
+    long_description=README,
+    url='https://github.com/fopina/py-tgbot',
+    author='Filipe Pina',
+    author_email='fopina@skmobi.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ],
+    install_requires=[
+    ]
+)
